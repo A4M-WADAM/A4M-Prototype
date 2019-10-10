14eae5e23db5779b4144dc57d539bc94125799ed
A4M-WADAM <56405685+A4M-WADAM@users.noreply.github.com>
2019-10-10 19:37:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<html></html>
+<html>
+  <title>A4M-WADAM Prototype</title>
+  <head>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+  </head>
+  <body>
+    <b>Things in this protoype:</b>
+    <br>- Adblock Blocker
+    <br>- "I'm not a robot" captcha
+    <br>- Domain name + SSL certificate
+    <br><b>Things I can do in the future:</b>
+    <br>- Get Google ads (AND MONEY!!!!!)
+    <br>- Get website on Google search results
+    <br><form action="index.php" method="post">
+    <div class="g-recaptcha" data-sitekey="6Lfu-bwUAAAAAEdgfkMP1MBwf92k58GgAHpgIrld"></div>
+    <br>
+    <input type="submit" name="amazingsubmitbutton" value="Verify">
+    </form>
+  </body>
+</html>
