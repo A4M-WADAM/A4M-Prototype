b54162d90de767b76c462d2e9b316d4e1782d24b
A4M-WADAM <56405685+A4M-WADAM@users.noreply.github.com>
2019-10-18 17:22:44 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// Main JS script for A4M-WADAM
+// Copyright 2019 All rights reserved
+
+function footer() {
+  <hr>
+  //TODO green text to protect the work
+}
+
+function header() {
+  //TODO MAIN MENU THING
+  <br>
+}
