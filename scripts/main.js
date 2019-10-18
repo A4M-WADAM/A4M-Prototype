c2914a9ef1f63d3c1f9525b25b986c4343d92f5c
A4M-WADAM <56405685+A4M-WADAM@users.noreply.github.com>
2019-10-18 18:09:09 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,12 @@
 // Copyright 2019 All rights reserved
 
 function footer() {
-  <hr>
+  document.write("<hr>");
   //TODO green text to protect the work
+  //TODO links 2 ToS & Privacy policy
 }
 
 function header() {
   //TODO MAIN MENU THING
-  <br>
+  document.write("<br>");
 }
